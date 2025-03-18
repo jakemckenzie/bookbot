337caf8b0f13354eebc4c31fd4dc2ab121b8850e
Jake McKenzie <jake314@uw.edu>
2025-03-18 13:32:14 -0600

    added character counting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(filepath):
   with open(filepath) as f:
@@ -6,8 +6,12 @@ def get_book_text(filepath):
 
 def main():
   book_text = get_book_text("/home/jake/bookbot/books/frankenstein.txt")
+
   words_count = count_words(book_text)
   print(f"{words_count} words found in the document")
+
+  char_count = count_characters(book_text)
+  print(char_count)
 
 if __name__ == "__main__":
   main()
--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+  char_count = {}
+  lowercase_text = text.lower()
+  for char in lowercase_text:
+    char_count[char] = char_count.get(char,0) + 1
+  return char_count
