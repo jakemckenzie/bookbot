c41f112a07a4ace4ccfc6572934369ba7c8fbe62
Jake McKenzie <jake314@uw.edu>
2025-03-18 13:22:07 -0600

    moved count_words to stats

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import count_words
+
 def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
-
-def count_words(text):
-  words = text.split()
-  return len(words)
 
 def main():
   book_text = get_book_text("/home/jake/bookbot/books/frankenstein.txt")
